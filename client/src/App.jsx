950e805bab0e7111be6412f6206cab40494c7719
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-04-06 20:03:10 -0400

    add react router dom and initial client navbar routes setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/authentication/Login";
+import Signup from "./components/authentication/Signup";
+import Navbar from "./components/navigation/Navbar";
+import Home from "./components/static/Home";
 
 const App = () => {
   const [title, setTitle] = useState()
@@ -11,10 +16,15 @@ const App = () => {
 
 
   return (
-    <div>
-      { title }
-    </div>
-  );
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/signup' element={ <Signup /> } />
+        <Route path='/login' element={ <Login /> } />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
